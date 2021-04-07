cfb8d0efa507e4ee7e84ea86cb09f999ba3d3cae
Michael <Mlcorrieri@gmail.com>
2021-04-07 15:40:43 -0400

    Still need to finish hide comment

---
+++ src/components/CommentContainer.js
@@ -0,0 +1,16 @@
+import Comments from "./Comments";
+import React, { useState } from "react";
+
+// function CommentContainer(){
+
+//     function hideContainer(){
+
+//     }
+
+//     return(
+//         // <div>
+//             {/* <button onClick =function()></button>
+
+//         </div>
+//     ) */}
+// })
--- src/components/VideoMain.js
+++ src/components/VideoMain.js
@@ -1,9 +1,27 @@
-function VideoMain({videoInfo}) {
-    // console.log(videoInfo)
-    return (
-        <h1>HEADER</h1>
-        
-    ) 
+import React, { useState } from "react";
+
+function VideoMain({ videoInfo }) {
+  const [count, setCount] = useState(9210);
+  const [countDown, setCountDown] = useState(185);
+
+  function handleCountClick() {
+    setCount(count + 1);
+  }
+
+  function countDownClick() {
+    setCountDown(countDown + 1);
+  }
+
+  return (
+    <div>
+      <h2>{videoInfo.title}</h2>
+      <p>
+        {videoInfo.views} Views | Uploaded {videoInfo.createdAt}
+      </p>
+      <button onClick={handleCountClick}>{count}👍</button>
+      <button onClick={countDownClick}>{countDown}👎</button>
+    </div>
+  );
 }
 
 export default VideoMain;
